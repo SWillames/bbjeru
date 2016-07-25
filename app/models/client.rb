7ea977266e0a36152dc4d5086b58e599858e3c3b
SWillames <sergiowoc@gmail.com>
2016-07-25 17:14:05 -0300

    a

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,6 @@ class Client < ActiveRecord::Base
   validates_presence_of :name, :cpf, :location, :salary
   validates_length_of :cpf, is: 11
   validates_uniqueness_of :cpf
+  has_many :sales
+  has_many :products, through :sales
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	validates_presence_of :name, :mark, :price
-	
+	has_many :sales
+  has_many :clents, through :sales
 end
