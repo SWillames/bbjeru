0a14cc5e5d6d1665d7a72a28830888938444535d
SWillames <sergiowoc@gmail.com>
2016-07-25 19:20:27 -0300

    Modificação

---
+++ db/migrate/20160725215805_create_sales.rb
@@ -0,0 +1,25 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.float :value
+      t.references :client, index: true, foreign_key: true
+      t.string :installments
+
+      t.timestamps null: false
+    end
+
+    create_table :products do |t|
+      t.float :price
+      t.text :description
+      t.string :mark
+      t.string :name
+
+      t.timestamps null: false
+    end
+
+    create_table :products_sales, id: false do |t|
+      t.belongs_to :product, index: true, foreign_key: true
+      t.belongs_to :sale, index: true, foreign_key: true
+    end
+  end
+end
